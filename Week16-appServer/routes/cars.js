8e0ed2bf221eb379aef19729dbd8396dbcd81260
pjackson <p.j1052@hotmail.com>
2018-02-06 15:25:41 +0000

    upload week04

--- Week16-appServer/routes/cars.js
+++ Week16-appServer/routes/cars.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 
 var counter = 0;
-var cars = {1:{id:1, reg:'123MO123', make: 'ford', model: 'mondeo'}}
+var cars = {1:{id:1, reg:'123MO123', make: 'Ford', model: 'Mondeo'}};
 
 
 
@@ -21,7 +21,9 @@ router.get('/', function(req, res, next) {
 //Operation: FindById, URL: /car/:id, Method: GET
 router.get('/:id', function (req, res, next) {
     var id = req.param("id");
-    console.log("got param" + id);
+    console.log("got param "+id);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(cars[id]));
 });
 
 //Operation: Create, URL: /car/create, Method: POST
@@ -30,7 +32,7 @@ router.post('/create', function (req, res, next) {
     var car = req.body;
     // put into a database
     cars[car.id] = car;
-    res.send("created?")
+    res.sendFile("C:\\Users\\ITS\\WebstormProjects\\ITS2 Pearse\\Week16-appServer\\public\\index.html")
 });
 
 //Operation: Update, URL: /car/update/:id, Method: POST
@@ -40,6 +42,6 @@ router.post('/update/:id', function (req, res, next) {
 
 //Operation: Delete, URL: /car/:id, Method: DELETE
 router.delete('/:id', function (req, res, next) {
-    console.log()
-})
+    console.log("delete function")
+});
 module.exports = router;
